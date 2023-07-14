cda7d0ae5e92ae59c024ec213c4b9b5c4cf79f5a
Kimchan-creater <139104738+Kimchan-creater@users.noreply.github.com>
2023-07-14 15:09:30 +0900

    Update code18.py

--- code18.py
+++ code18.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 
 import streamlit as st
+from streamlit_option_menu import option_menu
 import streamlit.components.v1 as html
 from  PIL import Image
 import numpy as np
 import pandas as pd
+import plotly.express as px
 import io
 
+import plotly.express as px
 with st.echo("below"):
     from st_pages import Page, add_page_title, show_pages
 
