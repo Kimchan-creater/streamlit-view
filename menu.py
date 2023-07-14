4a75d8972de2faa1b8bccdf62990589e64467223
Kimchan-creater <139104738+Kimchan-creater@users.noreply.github.com>
2023-07-14 15:24:00 +0900

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,10 @@
+def show_pages(
+        [
+            Page("streamlit_app.py", "Home", "🏠"),
+            Page("test01.py", "페이지01", ":books:"),
+            #Page("example_four.py", "Example Four", "📖"),
+            #Page("example_two.py", "Example Two", "✏️"),
+            #Page("example_three.py"),
+            #Page("example_five.py", "Example Five", "🧰"),
+        ]
+    )
